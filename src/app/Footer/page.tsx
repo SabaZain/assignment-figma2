b6a451a5aec240d874a291e574daafa023cfa761
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-11-27 15:30:08 +0500

    Update page.tsx

--- src/app/Footer/page.tsx
+++ src/app/Footer/page.tsx
@@ -20,8 +20,8 @@ const Footer = () => {
             <input
             type='email'
             placeholder='Enter your email'
-            className='border border-[#000000] rounded text-[16px] font-normal p-[6px] pr-36 m-1' />
-           <button className='border border-[#000000] rounded text-[14px] font-normal p-[7px] pl-[265px] md:pl-1 lg:pl-1 m-1 hover:bg-[#000000] hover:text-[#FFFFFF]'>Subscribe</button>
+            className='border border-[#000000] rounded text-[16px] font-normal p-[6px] pr-24 m-1' />
+           <button className='border border-[#000000] rounded text-[14px] font-normal p-[7px] pl-[215px] md:pl-1 lg:pl-1 m-1 hover:bg-[#000000] hover:text-[#FFFFFF]'>Subscribe</button>
         </div>
         <div>
             <p className='text-[12px] text-center md:text-start lg:text-start'>By subscribing you agree to with our Privacy Policy</p>
@@ -29,7 +29,7 @@ const Footer = () => {
       </div>
       </div>
       <div className='pt-20 pb-10 m-3 ml-36 md:ml-8 lg:ml-8  mr-24 flex flex-col md:flex-row lg:flex-row justify-between gap-2'>
-        <div className='ml-16'> 
+        <div className='ml-14'> 
         <Image src={"/Image/Ddsgnr.svg"} alt={"union logo"} width={90} height={30} />
         </div>
 
